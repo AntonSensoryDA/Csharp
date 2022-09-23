9a52bb1ae139f487812eb1629778673d2ec35038
anton.l.aleksandrovich <anton.sensory.analysis@gmail.com>
2022-09-23 17:06:14 +0300

    C#_1

---
+++ C#seminar_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// програма на вход число, на выход квадрать его
+Console.Clear();
+Console.Write("Пожалуйста введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+Console.WriteLine($"Квадрат числа {num}: {result}");
+//Console.WriteLine("Квадрат числа:" +result);
+
+if (result <= 777)
+{
+    Console.Write("Увы ");
+}
+else
+{
+    Console.Write("Поехало ");
+}
