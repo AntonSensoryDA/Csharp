fe9cacab0798c8556694f97ab956fbdb4fead4d8
anton.l.aleksandrovich <anton.sensory.analysis@gmail.com>
2022-10-21 21:20:15 +0300

    дополнил задачей в лекции

--- Lections/lection_2FunctMass/Task_4ArrayLibrary_2/Program.cs
+++ Lections/lection_2FunctMass/Task_4ArrayLibrary_2/Program.cs
@@ -21,10 +21,33 @@ void PrintArray (int[] col)
     }
 
 }
-// 37-я минута видео лекции
+// создаем метод. Так как он должен возвращать значение - поэтому не void. 
+int PoiskI (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+// в int position поставим -1 вместо 0, чтобы при поиске элемента массива, которого нет (например, 560 в массиве от 1 до 10 - нам выводилось -1 - означает, что такого нет)
+    while (index < count)
+    {
+        if (collection [index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 
 int[] array = new int[10];
 
 
 FillArray (array);
+// array [4] = 4;
+// array [6] = 4;-принудительно, сами добавляем в массив 4 на позиции 4 и 6 (чтобы показать, что break выводит первую найденную)
 PrintArray (array);
+Console.WriteLine();
+
+int pos = PoiskI(array, 5);
+Console.WriteLine(pos);
