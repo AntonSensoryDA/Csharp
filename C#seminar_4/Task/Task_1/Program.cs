e7477275a4727c3250c3a79d5c89ec728c2ffc8a
anton.l.aleksandrovich <anton.sensory.analysis@gmail.com>
2022-10-27 16:20:53 +0300

    папки семинар 4

---
+++ C#seminar_4/Task/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Количество знаков во входящем числе
+
+int SumFromToN (int number)
+{
+    int count = 1;
+    for (int i = 1; i < number; i++)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+int number = 45566;
+Console.WriteLine(SumFromToN(number));
+
---
+++ C#seminar_4/Task/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+string Reverse (string number)
+{
+    string result = String.Empty;
+    char[] ar = number.ToCharArray();
+    Array.Reverse(ar);
+    result = new String(ar);
+    return result;
+}
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+string res = Reverse(number);
+
+if (number == res) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
